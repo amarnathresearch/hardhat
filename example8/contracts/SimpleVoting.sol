b646d73b050d808b1552dc0f87665d7db6573519
Amarnath <amarnathresearch@gmail.com>
2025-05-26 18:40:42 +0530

    voting changes

--- example8/contracts/SimpleVoting.sol
+++ example8/contracts/SimpleVoting.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 contract SimpleVoting {
-    struct Candidate{
+    struct Candidate {
         string name;
         uint voteCount;
     }
@@ -12,33 +12,34 @@ contract SimpleVoting {
     uint public voteDeadline;
 
     constructor(string[] memory candidateNames, uint durationInMins) {
-        for(uint i=0; i<candidateNames.length; i++){
-            candidates.push(Candidate({name: candidateNames[i], voteCount = 0}));
+        require(candidateNames.length > 0, "Must provide at least one candidate");
+        for (uint i = 0; i < candidateNames.length; i++) {
+            candidates.push(Candidate({name: candidateNames[i], voteCount: 0}));
         }
-        voteDeadline = block.timestamp+durationInMins;
+        voteDeadline = block.timestamp + durationInMins * 1 minutes;
     }
 
-}
+    function vote(uint candidateIndex) external {
+        require(block.timestamp <= voteDeadline, "Voting has ended");
+        require(!hasVoted[msg.sender], "Already voted");
+        require(candidateIndex < candidates.length, "Invalid candidate");
 
-function vote(uint candidateIndex) external {
-    require(block.timestamp <= voteDeadline, "Voting has ended");
-    require(!hasVoted[msg.sender], "Already voted");
-    require(candidateIndex < candidates.length, "Invalid vote");
-    hasVoted[msg.sender] = true; // I have voted
-    candidates[candidateIndex].voteCount++;
-
-}
-
-function getWinner() public view returns (uint candidateIndex, string memory name){
-    require(block.timestamp > voteDeadline, "Voting still in process");
-    highestIndex = -1;
-    for(uint i =0; i<candidates.length; i++){
-        if(candidates[i].voteCount > highestIndex){
-            highestIndex = candidates[i].voteCount
-            candidateIndex = i;
-            name = candidates[i].name;
-        }
+        hasVoted[msg.sender] = true;
+        candidates[candidateIndex].voteCount++;
     }
 
-}
+    function getWinner() public view returns (uint candidateIndex, string memory name) {
+        require(block.timestamp > voteDeadline, "Voting is still ongoing");
 
+        uint highestVotes = 0;
+        candidateIndex = 0;
+
+        for (uint i = 0; i < candidates.length; i++) {
+            if (candidates[i].voteCount > highestVotes) {
+                highestVotes = candidates[i].voteCount;
+                candidateIndex = i;
+                name = candidates[i].name;
+            }
+        }
+    }
+}
